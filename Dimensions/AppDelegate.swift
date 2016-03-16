4ceef00b3b479d41be8371beb695b63930a75aa4
emrys57 <e.williams@linkz-im.com>
2016-03-16 15:15:18 +0000

    Initial Commit

---
+++ Dimensions/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Dimensions
+//
+//  Created by Emrys Williams on 2016/03/16.
+//  Copyright © 2016 Emrys Williams. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Dimensions/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Dimensions
+//
+//  Created by Emrys Williams on 2016/03/16.
+//  Copyright © 2016 Emrys Williams. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
